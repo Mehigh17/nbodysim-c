cf6d89883d8493095178fdb726dbaf515b8745cc
STAN Mihai <stanmihai17cs@gmaiL.com>
2017-11-20 20:25:01 +0100

    Updated the shaders to use version 3.0, also the code has been updated and removed unnecessary deprecated stuff.

--- main.c
+++ main.c
@@ -71,17 +71,18 @@ int main()
 
 	glGenBuffers(2, vbo);
 
-	glEnableVertexAttribArray(0);
-	glEnableVertexAttribArray(1);
-
 	// Coordinates VBO
 	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
 	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(GLfloat), triangle, GL_STATIC_DRAW); // Will have to use GL_DYANMIC_DRAW for moving shapes!
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(0);
 	// ^ Finished transferring the vertices coordinates to the GPU memory
 
 	// Colors VBO
 	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
 	glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(GLfloat), colors, GL_STATIC_DRAW);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(1);
 	// ^ Finished transferring the colors coordinates to the GPU memory
 
 	// Loading & Compiling shades
@@ -93,10 +94,19 @@ int main()
 	glAttachShader(program, fragment_shader);
 	// 
 
-	/*glBindAttribLocation(program, 0, "in_Position");
-	glBindAttribLocation(program, 1, "in_Color");*/
-
 	glLinkProgram(program);
+	// Linking verification
+	GLuint is_linked;
+	glGetProgramiv(program, GL_LINK_STATUS, &is_linked);
+	if(is_linked == GL_FALSE)
+	{
+		GLuint maxLen;
+		char* log = malloc(1000);
+		glGetProgramInfoLog(program, 1000, &maxLen, log);
+		printf("Program linking error:\n%s", log);
+	}
+	//
+
 	glUseProgram(program);
 
 	while(!glfwWindowShouldClose(window))
@@ -104,7 +114,7 @@ int main()
 		glfwGetFramebufferSize(window, &width, &height);
 		glViewport(0, 0, width, height);
 
-		compute_movement();
+		//compute_movement();
 
 		glClearColor(0.0, 0.0, 0.0, 1.0);
 		glClear(GL_COLOR_BUFFER_BIT);
@@ -114,6 +124,9 @@ int main()
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
+
+	/*const GLubyte* version = glGetString(GL_SHADING_LANGUAGE_VERSION);
+	printf("GL Version: %s\n", version);*/
 
 	glfwDestroyWindow(window);
 
@@ -158,7 +171,7 @@ GLuint get_shader(char* source_code, GLenum type)
 	{
 		int max_len = 1000;
 		int log_len;
-		char* log = malloc(max_len); // max_len bytes
+		char* log = malloc(max_len);
 
 		glGetShaderInfoLog(shader, max_len, &log_len, log);
 
@@ -191,16 +204,13 @@ char* read_file(char* file_name)
 	long size = ftell(file);
 
 	char* source = (char*) malloc(size + 1);
-	*(source + size + 1) = '\0';
+	source[size + 1] = '\0';
 
 	rewind(file); // file stream goes back to first position
 
-	for(int i = 0; i < size; i++)
-	{
-		*(source + i) = fgetc(file);
-	}
+	fread(source, sizeof(char), size, file);
 
-	//printf("File %s has a size of %d bytes.\n", file_name, size);
+	//printf("File %s has a size of %d bytes.\nSource code:\n%s", file_name, size, source);
 
 	fclose(file);
 
